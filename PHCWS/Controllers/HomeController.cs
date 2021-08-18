23d01e75410f991c2a5780bb27a9e4317035bc4f
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-08-18 11:12:49 +0000

    Added API action method and view

--- PHCWS/Controllers/HomeController.cs
+++ PHCWS/Controllers/HomeController.cs
@@ -1,33 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PHCWS.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			//ViewBag.Title = "Home Page";
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.Title = "Home Page";
 
-			return View();
-		}		public ActionResult About()
-		{
-			//ViewBag.Title = "Home Page";
+            return View();
+        }
+        public ActionResult API()
+        {
+            //ViewBag.Title = "Home Page";
 
-			return View();
-		}		public ActionResult Service()
-		{
-			//ViewBag.Title = "Home Page";
+            return View();
+        }
+        public ActionResult About()
+        {
+            //ViewBag.Title = "Home Page";
 
-			return View();
-		}		public ActionResult Contact()
-		{
-			//ViewBag.Title = "Home Page";
+            return View();
+        }
+        public ActionResult Service()
+        {
+            //ViewBag.Title = "Home Page";
 
-			return View();
-		}
-	}
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            //ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
 }
