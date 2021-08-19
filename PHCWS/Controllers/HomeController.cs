60153b0175a960999d2133338194bf39c03cc2e4
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-08-19 09:43:10 +0000

    Fixed menu spacing.

--- PHCWS/Controllers/HomeController.cs
+++ PHCWS/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Diagnostics;
+using System.Web.Mvc;
 
 namespace PHCWS.Controllers
 {
@@ -7,31 +8,31 @@ namespace PHCWS.Controllers
         public ActionResult Index()
         {
             //ViewBag.Title = "Home Page";
-
+            Debug.Write("Welcome to Index. ");
             return View();
         }
         public ActionResult API()
         {
             //ViewBag.Title = "Home Page";
-
+            Debug.Write("Welcome to API. ");
             return View();
         }
         public ActionResult About()
         {
             //ViewBag.Title = "Home Page";
-
+            Debug.Write("Welcome to About. ");
             return View();
         }
         public ActionResult Service()
         {
             //ViewBag.Title = "Home Page";
-
+            Debug.Write("Welcome to Service. ");
             return View();
         }
         public ActionResult Contact()
         {
             //ViewBag.Title = "Home Page";
-
+            Debug.Write("Welcome to Contact. ");
             return View();
         }
     }
